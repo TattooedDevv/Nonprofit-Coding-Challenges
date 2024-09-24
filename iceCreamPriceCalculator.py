387475b6e6a9da0d80cd5e474c4440b4da5b30c5
callmejazz <jmeade4347@gmail.com>
2024-09-24 12:47:34 -0400

    Create iceCreamPriceCalculator.py
    This is the starter code to the challenge

---
+++ iceCreamPriceCalculator.py
@@ -0,0 +1,9 @@
+print("Welcome to the ice cream shop! ")
+container = input("What container would you like? Cone or Cup ")
+scoops = input("How many scoops would you like?: Pick between 1 -4")
+optionalToppings = input("Would you like extra toppings?: Pick between Flakes, Strawberry Sprinkles, Chocolate Sprinkles or none")
+
+#Cone:80, Cup:50, scoops:1 each, Flakes:40, Chocolate sprinkles:30, Strawberry Sprinkles:60
+
+#todo: you have a customer wanting ice cream, write a program that will calculate the price of their order and output the total
+
